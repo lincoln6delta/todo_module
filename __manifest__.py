292f76a870cd78531b58a1a9a7fbb59aa527cd13
Rodulfo Guerrero Jr <guerrero@businessitphilippines.com>
2019-12-06 18:55:28 +0800

    first push

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'To do module',
+    'version': '11.0.1.1',
+    'category': 'Extra Tools',
+    'description': """
+        To do module
+        
+        """,
+    'summary': 'To do module',
+    'author': 'Agile Business Technologies',
+    'website': 'http://abt.ph/',
+    'depends': ['sale_stock'],
+    'data': [
+    ],
+    'installable': True,
+}
+
